262487057d228d8d4064b792ae7060907f3106c7
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-01-23 19:07:51 +0100

    Additive copy

--- scrabble/consumers.py
+++ scrabble/consumers.py
@@ -13,7 +13,7 @@ class PlayerConsumer(AsyncWebsocketConsumer):
         self.room_id = self.scope['url_route']['kwargs']['room_id']
         self.room_group_name = 'room_%s' % self.room_id
 
-        room_instance = await database_sync_to_async(Room.objects.get)(id=self.room_id)
+        room_instance: Room = await database_sync_to_async(Room.objects.get)(id=self.room_id)
         user = self.scope["user"]
         if not await (user.is_authenticated and sync_to_async(room_instance.join)(user)):
             return
@@ -25,6 +25,13 @@ class PlayerConsumer(AsyncWebsocketConsumer):
         )
 
         await self.accept()
+
+        if await sync_to_async(room_instance.is_full)():
+            if await sync_to_async(room_instance.is_in_progress)():
+                await self.send_letters_for_player()
+            else:
+                await self.send_new_letters()
+            await self.send_board()
 
     async def disconnect(self, close_code):
         room_instance = await database_sync_to_async(Room.objects.get)(id=self.room_id)
@@ -40,24 +47,22 @@ class PlayerConsumer(AsyncWebsocketConsumer):
         action = text_data_json['action']
 
         if action == "request_letters":
-            pass
-
-        # Send action to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'action',
-                'action': action
-            }
-        )
+            await self.send_new_letters()
 
     async def set_letters(self, event):
-        target: User = event['user']
-        if not target == self.scope["user"]:
+        target = event['user_id']
+        if not target == self.scope["user"].id:
             return
 
         await self.send(text_data=json.dumps({
+            'operation': event['operation'],
             'letters': event['letters']
+        }))
+
+    async def set_board(self, event):
+        await self.send(text_data=json.dumps({
+            'operation': event['operation'],
+            'board': event['board']
         }))
 
     # Receive message from room group
@@ -69,3 +74,55 @@ class PlayerConsumer(AsyncWebsocketConsumer):
             'message': message
         }))
 
+    async def send_board(self):
+        room_instance: Room = await database_sync_to_async(Room.objects.get)(id=self.room_id)
+        board = await sync_to_async(room_instance.get_board)()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'set_board',
+                'operation': 'set_board',
+                'board': board
+            }
+        )
+
+    async def send_new_letters(self):
+        room_instance: Room = await database_sync_to_async(Room.objects.get)(id=self.room_id)
+        letters = await sync_to_async(room_instance.pass_new_letters)()
+        p1_id = await sync_to_async(room_instance.get_player_1_id)()
+        p2_id = await sync_to_async(room_instance.get_player_2_id)()
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'set_letters',
+                'operation': 'set_letters',
+                'user_id': p1_id,
+                'letters': letters['player_1']
+            }
+        )
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'set_letters',
+                'operation': 'set_letters',
+                'user_id': p2_id,
+                'letters': letters['player_2']
+            }
+        )
+
+    async def send_letters_for_player(self):
+        room_instance: Room = await database_sync_to_async(Room.objects.get)(id=self.room_id)
+        user: User = self.scope["user"]
+        letters = await sync_to_async(room_instance.get_letters_for_player)(user)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'set_letters',
+                'operation': 'set_letters',
+                'user_id': user.id,
+                'letters': letters
+            }
+        )
--- scrabble/models.py
+++ scrabble/models.py
@@ -1,5 +1,4 @@
 import random
-import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -18,12 +17,31 @@ class Room(models.Model):
     player1 = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="p1")
     player2 = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name="p2")
 
-    letters = models.TextField(null=True)
-    player1_letters = models.TextField(null=True)
-    player2_letters = models.TextField(null=True)
+    letters = models.TextField(default="")
+    player1_letters = models.TextField(default="")
+    player2_letters = models.TextField(default="")
 
-    board = models.TextField(null=True)
+    board = models.TextField(default="")
     size = models.IntegerField(default=15)
+
+    in_progress = models.BooleanField(default=False)
+
+    def get_player_1_id(self):
+        return self.player1.id
+
+    def get_player_2_id(self):
+        return self.player2.id
+
+    def get_letters_for_player(self, player):
+        if self.player1 == player:
+            return self.player1_letters
+        elif self.player2 == player:
+            return self.player2_letters
+        else:
+            return "BAD PLAYER"
+
+    def get_board(self):
+        return self.board
 
     def join(self, user):
         if self.player1 == user or self.player2 == user:
@@ -36,8 +54,19 @@ class Room(models.Model):
         else:
             return False
 
+        if self.player1 is not None and self.player2 is not None:
+            self.reset_board()
+            self.reset_letters()
+            self.in_progress = True
+
         self.save()
         return True
+
+    def is_full(self):
+        return self.player1 is not None and self.player2 is not None
+
+    def is_in_progress(self):
+        return self.in_progress
 
     def leave(self, user):
         if self.player1 == user:
@@ -65,19 +94,24 @@ class Room(models.Model):
     def pass_new_letters(self):
         p1_deficit = 8 - len(self.player1_letters)
         p2_deficit = 8 - len(self.player2_letters)
+        p1_new = ""
+        p2_new = ""
 
         for i in range(p1_deficit):
             if len(self.letters) <= 0:
                 break
 
-            self.player1_letters += self.letters[0]
+            p1_new += self.letters[0]
             self.letters = self.letters[1:]
+        self.player1_letters += p1_new
 
         for i in range(p2_deficit):
             if len(self.letters) <= 0:
                 break
 
-            self.player2_letters += self.letters[0]
+            p2_new += self.letters[0]
             self.letters = self.letters[1:]
+        self.player2_letters += p2_new
 
         self.save()
+        return {'player_1': p1_new, 'player_2': p2_new}
